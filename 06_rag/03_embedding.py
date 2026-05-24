5bb2b6cee92d7773f5d4e88d32dfde2446656fd9
Fchen <fc_2012@163.com>
2026-05-24 10:48:56 +0800

    feat: 向量嵌入

---
+++ 06_rag/03_embedding.py
@@ -0,0 +1,22 @@
+# 嵌入
+# 嵌入是表示学习的一种形式，通常用于将高纬度数据映射到低微空间中的表示形式。嵌入可以是词嵌入，图像嵌入等。
+
+from openai import OpenAI
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv(verbose=True)
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"),base_url=os.getenv("OPENAI_API_URL"))
+
+def get_embedding(text, model = "text-"):
+
+    data = client.embeddings.create(input=text, model=model).data
+
+    return [x.embedding for x in data]
+
+
+vec = get_embedding(["苹果"])
+print(f'第一个向量 {vec[0]}')
+print(f'向量的纬度 {len(vec)}')
